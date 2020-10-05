d5954c8095ae477b8f2def3eea87106b0c0ca5e6
Ghassen Rjab <rjab.ghassen@gmail.com>
2020-10-05 02:46:45 +0200

    Add 2.7 solution

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -12,7 +12,12 @@ const App = () => {
 
   const onFormSubmit = (event) => {
     event.preventDefault();
-    setPersons(persons.concat({ name: newName }));
+    if (persons.find(person => person.name === newName)) {
+      alert(`${newName} is already added to phonebook`);
+    } else {
+      setPersons(persons.concat({ name: newName }));
+      setNewName('');
+    }
   }
 
   return (
